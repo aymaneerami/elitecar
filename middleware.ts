ffa5f4490c43fabb50941ece00859762c3266174
Aymane erami <aymaneerami2@gmail.com>
2026-08-28 18:12:03 +0100

    ovep

--- middleware.ts
+++ middleware.ts
@@ -13,11 +13,7 @@ export function middleware(request: NextRequest) {
   response.headers.set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
   
   // Content Security Policy
-  response.headers.set(
-  'Content-Security-Policy',
-  "default-src 'self' data: blob: https:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.gstatic.com https://connect.facebook.net https://hdbtop.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https: blob: https://www.facebook.com; connect-src 'self' https://api.resend.com https://www.facebook.com https://connect.facebook.net https://hdbtop.com; frame-src 'self' https://www.google.com;"
-);
-
+ 
   return response;
 }
 
